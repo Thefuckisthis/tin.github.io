caecbbbc786109b73a34e75014a03e726e0b900a
Chard <161943504+Thefuckisthis@users.noreply.github.com>
2025-10-23 12:32:28 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const modal = document.getElementById("testimonialModal");
+const openBtn = document.querySelector(".open-modal");
+const closeBtn = document.querySelector(".close");
+
+openBtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
